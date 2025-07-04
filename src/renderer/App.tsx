53e4ce20ef449468a5247316264bab67a158fad2
Udo <writeudo@gmail.com>
2025-07-04 17:15:22 +0100

    update ui

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,7 +6,8 @@ import './App.css';
 import { createTheme, MantineProvider } from '@mantine/core';
 
 const theme = createTheme({
-  fontFamily: 'Nunito, sans-serif',
+  fontFamily:
+    'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
 });
 
 export default function App() {
