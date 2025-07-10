bb2e9e6ccef0bc3ed384913f5f723560540d84df
creativecoderjoy <154061032+creativecoderjoy@users.noreply.github.com>
2025-07-10 22:52:47 +0100

    add side menu with  funtionality to add a new subject

--- src/renderer/SideMenu.tsx
+++
@@ -1,9 +0,0 @@
-const SideMenu = () => {
-  return (
-    <div>
-      <h1>Side Menu</h1>
-    </div>
-  );
-};
-
-export default SideMenu;
---
+++ src/renderer/components/Dashboard.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import SideMenu from './SideMenu';
+import { APP_NAME } from '../../constants/misc';
+
+export default function Dashboard() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <div>{APP_NAME}</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">
+        <SideMenu />
+      </AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
+}
