7169c24e26b7c76cb5410081e6b539086bdfbb1c
creativecoderjoy <154061032+creativecoderjoy@users.noreply.github.com>
2025-07-16 10:51:18 +0100

    create question component

--- src/renderer/components/Main.tsx
+++ src/renderer/components/Main.tsx
@@ -4,12 +4,16 @@ import { Button, Stack, Text, Badge, Group } from '@mantine/core';
 import useFiles from '../hooks/useFiles';
 import { IconUpload } from '@tabler/icons-react';
 import File from './File';
+import Question from './Question';
 
 export default function Main() {
   const { selected } = useContext(SubjectContext);
   return selected == null ? (
     <div>Subject empty</div>
   ) : (
-    <File subject={selected} />
+    <>
+      <File subject={selected} />
+      <Question subject={selected} />
+    </>
   );
 }
---
+++ src/renderer/components/Question.tsx
@@ -0,0 +1,9 @@
+import { TSubject } from '../../types';
+
+type Props = {
+  subject: TSubject;
+};
+
+export default function Question({ subject }: Props) {
+  return <div>Question</div>;
+}
